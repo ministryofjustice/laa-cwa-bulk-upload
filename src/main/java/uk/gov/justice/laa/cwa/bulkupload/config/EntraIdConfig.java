2ba6d935e02737bdc1acc3ba49aac361bbf82ee1
Tosin <tosin.yusuf@digital.justice.gov.uk>
2025-05-23 10:36:13 +0100

    Added access token code

---
+++ src/main/java/uk/gov/justice/laa/cwa/bulkupload/config/EntraIdConfig.java
@@ -0,0 +1,59 @@
+package uk.gov.justice.laa.cwa.bulkupload.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+
+@Configuration
+public class EntraIdConfig {
+
+    @Value("${azure.activedirectory.client-id}")
+    private String clientId;
+
+    @Value("${azure.activedirectory.client-secret}")
+    private String clientSecret;
+
+    @Value("${azure.activedirectory.tenant-id}")
+    private String tenantId;
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(clientRegistration());
+    }
+
+    @Bean
+    public OAuth2AuthorizedClientService authorizedClientService(ClientRegistrationRepository clientRegistrationRepository) {
+        return new InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository);
+    }
+
+    @Bean
+    public OAuth2AuthorizedClientManager authorizedClientManager(ClientRegistrationRepository clientRegistrationRepository,
+                                                                 OAuth2AuthorizedClientService authorizedClientService) {
+        OAuth2AuthorizedClientProvider authorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder().clientCredentials().build();
+        AuthorizedClientServiceOAuth2AuthorizedClientManager authorizedClientManager
+                = new AuthorizedClientServiceOAuth2AuthorizedClientManager(clientRegistrationRepository, authorizedClientService);
+        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+        return authorizedClientManager;
+    }
+
+    private ClientRegistration clientRegistration() {
+        return ClientRegistration
+                .withRegistrationId("azure")
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .tokenUri(String.format("https://login.microsoftonline.com/%s/oauth2/v2.0/token", tenantId))
+                .scope("api://laa-sds-dev/.default")
+                .build();
+    }
+}
---
+++ src/main/java/uk/gov/justice/laa/cwa/bulkupload/service/TokenService.java
@@ -0,0 +1,42 @@
+package uk.gov.justice.laa.cwa.bulkupload.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+
+@Service
+public class TokenService {
+
+    private final OAuth2AuthorizedClientManager authorizedClientManager;
+    private OAuth2AccessToken accessToken;
+
+    @Autowired
+    public TokenService(OAuth2AuthorizedClientManager authorizedClientManager) {
+        this.authorizedClientManager = authorizedClientManager;
+    }
+
+    public String getAccessToken() {
+        if (accessToken != null && accessToken.getExpiresAt() != null &&
+                accessToken.getExpiresAt().isAfter(Instant.now().plusSeconds(60))) {
+            return accessToken.getTokenValue();
+        }
+
+        OAuth2AuthorizeRequest authorizeRequest = OAuth2AuthorizeRequest
+                .withClientRegistrationId("azure")
+                .principal("service")
+                .build();
+
+        OAuth2AuthorizedClient authorizedClient = authorizedClientManager.authorize(authorizeRequest);
+        if (authorizedClient == null || authorizedClient.getAccessToken() == null) {
+            throw new RuntimeException("Failed to obtain access token");
+        }
+
+        accessToken = authorizedClient.getAccessToken();
+        return accessToken.getTokenValue();
+    }
+}
