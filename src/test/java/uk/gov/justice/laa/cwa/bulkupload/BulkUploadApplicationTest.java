5c970228415884e632fbca15bc315dc02f0089e9
Tosin <tosin.yusuf@digital.justice.gov.uk>
2025-05-30 16:41:03 +0100

    Fixed test

--- src/test/java/uk/gov/justice/laa/cwa/bulkupload/BulkUploadApplicationTest.java
+++ src/test/java/uk/gov/justice/laa/cwa/bulkupload/BulkUploadApplicationTest.java
@@ -6,11 +6,15 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(properties = "spring.profiles.active=test")
+@SpringBootTest
+@ActiveProfiles("test")
 class BulkUploadApplicationTest {
+
+    // Disable spring security
     @TestConfiguration
-    public static class WebClientConfiguration {
+    public static class TestSecurityConfig {
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
             http.authorizeHttpRequests(/**/authorize -> authorize
