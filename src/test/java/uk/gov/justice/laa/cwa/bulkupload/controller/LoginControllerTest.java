6a413a3bb27db87bbb68db73d014bdc03c4749ed
Solomanik <solomon.manikyam@justice.gov.uk>
2025-06-24 15:45:08 +0100

    fix junit test

--- src/test/java/uk/gov/justice/laa/cwa/bulkupload/controller/LoginControllerTest.java
+++ src/test/java/uk/gov/justice/laa/cwa/bulkupload/controller/LoginControllerTest.java
@@ -2,6 +2,7 @@ package uk.gov.justice.laa.cwa.bulkupload.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -10,6 +11,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @WebMvcTest(LoginController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class LoginControllerTest {
 
     @Autowired
