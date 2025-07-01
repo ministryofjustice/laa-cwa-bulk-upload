04dae25d2e32805798cd22cde01c7b029ae4ee7b
Solomanik <solomon.manikyam@justice.gov.uk>
2025-07-01 09:06:16 +0100

    Fix the tests issues

--- src/test/java/uk/gov/justice/laa/cwa/bulkupload/BulkUploadApplicationTest.java
+++ src/test/java/uk/gov/justice/laa/cwa/bulkupload/BulkUploadApplicationTest.java
@@ -3,7 +3,8 @@ package uk.gov.justice.laa.cwa.bulkupload;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+@SpringBootTest(classes = BulkUploadApplicationTest.class)
 class BulkUploadApplicationTest {
     @Test
     void contextLoads() {
